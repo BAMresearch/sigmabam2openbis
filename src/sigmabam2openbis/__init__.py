a6b81e26e6d8ba737830817628fb605b68cff031
jpizarro <jose.pizarro-blanco@bam.de>
2025-09-26 14:44:17 +0200

    New files and structure

---
+++ src/sigmabam2openbis/__init__.py
@@ -0,0 +1,8 @@
+from .parser import SigmaAtBAMParser
+
+# Add more metadata if needed
+sigmaatbam_parser_entry_point = {
+    "name": "SigmaAtBAMParser",
+    "description": "An example parser for masterdata.",
+    "parser_class": SigmaAtBAMParser,
+}
